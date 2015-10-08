c2c89062c8ec93dd5a71cc1eb9e98958f6409594
jobinuw <jmmn84@hotmail.com>
2015-10-08 18:55:57 -0430

    Jscript added

---
+++ Js/js.js
@@ -0,0 +1,32 @@
+if ("mye".length >= 7) 
+{
+    console.log("Let's go down the first road!");
+}
+else 
+{
+    // What should we do if the condition is false? Fill in here:
+     console.log("Nombre muy corto");
+}
+
+if ("mye".length >= 7) 
+{
+   console.log("The condition is true");
+}
+else // "otherwise"
+{
+    console.log("The condition is False");
+}
+
+console.log("Xiao Hui".length <122);
+console.log("Goody Donaldson".length > 8);
+console.log(8*2===16);
+
+// Ventanas Pop-ups
+confirm("I am ok");
+prompt("Are you ok?");
+
+if (12 / 4 === "Ari".length) {
+    confirm("Will this run the first block?");
+} else {
+    confirm("Or the second block?");
+}
