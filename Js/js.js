5c6513fbb1c4a0202f0c36b0c91c2946d8cf1d78
jobinuw <jmmn84@hotmail.com>
2015-11-04 21:48:56 -0430

    Objetos Funciones y Constructores

--- Js/js.js
+++ Js/js.js
@@ -670,6 +670,61 @@ var susan = new Person("Susan Jordan", 25);
 // help us make george, whose name is "George Washington" and age is 275
 var george = new Person("George Washington", 275);
 
+///////////////////////////////////////////////////////////////////////////////
+//EJEMPLO CONSTRUCTOR
+
+function Person(name,age) {
+  this.name = name;
+  this.age = age;
+  this.species = "Homo Sapiens";
+}
+
+var sally = new Person("Sally Bowles", 39)
+var holden = new Person("Holden Caulfield", 16)
+console.log("sally's species is " + sally.species + " and she is " + holden.age);
+console.log("holden's species is " + holden.species + " and he is " + sally.age);
+
+
+///////////////////////////////////////////////////////////////////////////////////
+FUNCIONES DENTRO DEL CONSTRUCTOR
+///////////////////////////////////////////////////////////////////////////////////
+
+function Rectangle(height, width) {
+  this.height = height;
+  this.width = width;
+  this.calcArea = function() {
+      return this.height * this.width;
+  };
+  // put our perimeter function here!
+  this.calcPerimeter = function() {
+      return ((this.height * 2)+(this.width * 2));
+  };
+}
+
+var rex = new Rectangle(7,3);
+var area = rex.calcArea();
+var perimeter = rex.calcPerimeter();
+console.log(rex)
+console.log(area)
+console.log(perimeter)
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+// MAS OBJETOS FUNCIONES Y CONSTRUCTOR
+function Rabbit(adjective) {
+    this.adjective = adjective;
+    this.describeMyself = function() {
+        console.log("I am a " + this.adjective + " rabbit");
+    };
+}
+
+// now we can easily make all of our rabbits
+var rabbit1 = new Rabbit("fluffy");
+var rabbit2= new Rabbit("happy");
+var rabbit3 = new Rabbit("sleepy");
+rabbit1.describeMyself()
+rabbit2.describeMyself()
+rabbit3.describeMyself()
 
 
 
