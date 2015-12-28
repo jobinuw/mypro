f9c6ce7f2a2c994dfcc4616c7961578346c0f9fc
jobinuw <jmmn84@hotmail.com>
2015-12-28 13:49:39 -0430

    avance codecademy js

--- Js/js.js
+++ Js/js.js
@@ -979,9 +979,88 @@ var snoopy = new Dog("Beagle");
 snoopy.bark();
 =====================================================================================
 
+function Person(name,age) {
+  this.name = name;
+  this.age = age;
+}
+// a function that prints the name of any given person
+var printPersonName = function (p) {
+  console.log(p.name);
+};
+
+var bob = new Person("Bob Smith", 30);
+printPersonName(bob);
+
+// make a person called me with your name and age
+// then use printPersonName to print your name
+var me = new Person("Jose", 31);
+printPersonName(me);
+
+=========================================================================================
+
+function Dog (breed) {
+  this.breed = breed;
+};
+
+// here we make buddy and teach him how to bark
+var buddy = new Dog("golden Retriever");
+Dog.prototype.bark = function() {
+  console.log("Woof");
+};
+buddy.bark();
+
+// here we make snoopy
+var snoopy = new Dog("Beagle");
+/// this time it works!
+snoopy.bark();
+
+=========================================================================================
+
+// create your Animal class here
+
+function Animal(name,leg) {
+  this.name = name;
+  this.numLegs = leg;
+}
+
+// create the sayName method for Animal
+
+Animal.prototype.sayName = function() {
+  console.log("Hi my name is " + this.name);
+};
 
 
 
+// provided code to test above constructor and method
+var penguin = new Animal("Captain Cook", 2);
+penguin.sayName();
+penguin.numLegs
+
+==============================================================================================
+//Inheritance    Herencia
+
+// the original Animal class and sayName method
+function Animal(name, numLegs) {
+    this.name = name;
+    this.numLegs = numLegs;
+}
+Animal.prototype.sayName = function() {
+    console.log("Hi my name is " + this.name);
+};
+
+// define a Penguin class
+function Penguin(name) {
+    this.name = name;
+    this.numLegs = 2;
+}
+// set its prototype to be a new instance of Animal
+Penguin.prototype = new Animal();
+var Pen = new Penguin("cirilo");
+
+
+Pen.sayName();
+
+=============================================================================================
 
 
 
